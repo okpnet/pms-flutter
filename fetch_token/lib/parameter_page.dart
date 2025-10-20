1dc3928f4eaa8cd7dc9c01f044728fa1af5d4382
hayato <umai-beer@hotmail.co.jp>
2025-10-20 17:34:13 +0900

    ダイアログOK。 フィールドウィジットへプログラムから反映する場合は、Controllerプロパティへインスタンスをセットする。

--- fetch_token/lib/parameter_page.dart
+++ fetch_token/lib/parameter_page.dart
@@ -2,12 +2,15 @@
 import 'package:fetch_token/file_helper.dart';
 import 'package:fetch_token/models/parameter_model.dart';
 import 'package:fetch_token/pkce_proviers_struct.dart';
+import 'package:fetch_token/widgets/widget_providers_struct.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pms_pkce_lib/providers/pms_provider.dart';
 
 /// プロバイダ構造体
 final providers= PkceProviersStruct();
+/// ウィジェットプロバイダ構造体
+final widgetProviders= WidgetProvidersStruct();
 
 ApiUrlConfig defaultApiUrlConfig = ApiUrlConfig(
   baseUrl: 'https://example.com',
@@ -23,14 +26,15 @@ class ParameterPage extends ConsumerStatefulWidget {
 }
 
 class ParameterPageState extends ConsumerState<ParameterPage> {
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    
     final parameter=ref.watch(providers.parameterModelProvider);
+    final appbar=ref.watch(widgetProviders.appBarProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Parameter Page'),
-      ),
+      appBar: appbar,
       body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -75,7 +79,37 @@ class ParameterPageState extends ConsumerState<ParameterPage> {
             parameter.clientId = value;
           },
         ),
-        // SizedBox(height: 16),
+        SizedBox(height: 16),
+        Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded( // ← これが重要
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Cert File Path',
+                ),
+                onChanged: (value) {
+                  parameter.certFilePath = value;
+                },
+              ),
+            ),
+            const SizedBox(width: 8), // 間隔を空けると見やすくなります
+            ElevatedButton(
+              onPressed: () async {
+                      FileHelper.pickKmlFile(['crt', 'cer', 'pem']).then((result) {
+                        if (result == null || result.count == 0) return;
+                        setState(() {
+                          parameter.certFilePath = result.paths.first;
+                          _controller.text = result.paths.first??'';
+                        });
+                      });
+                    },
+              child: const Text("select file"),
+            ),
+          ],
+        ),
         // Row(
         //   children: [
         //     TextField(
@@ -104,7 +138,8 @@ class ParameterPageState extends ConsumerState<ParameterPage> {
 
         SizedBox(height: 16),
         ElevatedButton(
-          onPressed: () async {
+          onPressed:
+           !parameter.isValid? null:() async {
             // ApiUrlConfig config = await parameter.convrt();
             // // Handle the button press, e.g., save parameters or fetch token
             // print('Base URL: ${config.baseUrl}');
--- fetch_token/lib/widgets/widget_providers_struct.dart
+++ fetch_token/lib/widgets/widget_providers_struct.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/legacy.dart';
 class WidgetProvidersStruct {
-  
+  late final StateProvider<ThemeData> themeProvider;
+  late final StateProvider<AppBar> appBarProvider;
+
+  WidgetProvidersStruct(){
+    themeProvider = StateProvider<ThemeData>((ref){
+      return ThemeData.dark();
+    });
+    appBarProvider = StateProvider<AppBar>((ref){
+      final theme = ref.watch(themeProvider);
+      return AppBar(
+        title: Text('Default Title'),
+        backgroundColor: theme.primaryColor,
+        foregroundColor: theme.colorScheme.onPrimary,
+      );
+    });
+  }
 }
